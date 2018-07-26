dfcfe32941e9023083a90b641e14e210ab42734b
Ryan <azbo400@gmail.com>
2018-07-26 12:17:16 -0700

    make the navbar change colour

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 
 import { Link } from 'react-router-dom';
+import '../styles/App.css';
 
 class Navbar extends Component {
-  render() { 
+  state = {
+    isTop: true
+  };
+  
+  componentDidMount() {
+    document.addEventListener('scroll', () => {
+      const isTop = window.scrollY < 100
+      if (isTop !== this.state.isTop) {
+        this.setState({ isTop });
+      }
+    });
+  }
+
+  render() {
+    var navClass = this.state.isTop ? 'is-top' : 'not-top';
     return ( 
-      <div style={{padding: 10, display: 'flex', alignItems: 'center', position: 'fixed'}}>
+      <div style={{padding: 10, display: 'flex', alignItems: 'center', position: 'fixed', width: '100%'}} className={navClass}>
         <h2><Link style={{textDecoration: 'none', color: 'black'}} to="/">Machine Learning Introduction</Link></h2>
-        <Link to="/login" style={{textDecoration: 'none', color: 'black', position: 'fixed', right: '13vw'}}>Login</Link> 
-        <Link to="/signup" style={{textDecoration: 'none', backgroundColor: '#4C85FF', padding : 10, borderRadius: 10, color: 'white', position: 'fixed', right: '5vw'}}>Sign Up</Link>
+        <div style={{display: 'flex', alignItems: 'center'}} ref="navbar">
+          <Link to="/login" style={{textDecoration: 'none', color: 'black', position: 'fixed', right: '13%'}}>Login</Link> 
+          <Link to="/signup" style={{textDecoration: 'none', backgroundColor: '#4C85FF', padding : 10, borderRadius: 10, color: 'white', position: 'fixed', right: '5%'}}>Sign Up</Link>
+        </div>
       </div>
     );
   }
