f1d715e7bc72feb8e56f9c59d3e0b24666320b9e
Ryan <azbo400@gmail.com>
2018-08-26 20:32:22 -0700

    Worked on about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import ProfileNavbar from './ProfileNavbar';
 
 class About extends Component {
   render() { 
-    return ( 
-      <p>About page</p>
+    return (
+      <div style={{fontSize: 16, fontFamily: 'roboto'}}>
+        <ProfileNavbar style={{position: 'fixed'}}/>
+        <div style={{justifyContent: 'center', alignItems: 'center', display: 'flex', height: '80vh'}}>
+          <div style={{textAlign: 'center', width: '40vw'}}>
+            Developed by Ryan Arnouk 2018<br/>
+            Machine Learning Introduction is an app that uses block components to teach machine learning.<br/>
+            <div style={{marginTop: 20}}>
+              Version: 0.1<br />
+              Last Updated: 2018-08-26
+            </div>
+          </div>
+        </div>
+      </div> 
     );
   }
 }
