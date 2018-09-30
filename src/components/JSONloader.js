7b5a9a72c090d86990ac2a3bb5308f135daaec24
Ryan <azbo400@gmail.com>
2018-09-30 11:28:26 -0700

    made jsonloeader work for all the sections

--- src/components/JSONloader.js
+++ src/components/JSONloader.js
@@ -1,11 +1,20 @@
 import regression from '../seed/challenges/regression/lesson.json';
 import classification from '../seed/challenges/classification/lesson.json';
+import introcoding from '../seed/challenges/introcoding/lesson.json';
+import deeplearning from '../seed/challenges/deeplearning/lesson.json';
+import reinfocementlearning from '../seed/challenges/reinforcementlearning/lesson.json';
 
 function returnfile() {
   if(window.location.pathname.split('/')[2] === 'regression'){
     return regression; 
   } else if(window.location.pathname.split('/')[2] === 'classification') {
     return classification; 
+  } else if(window.location.pathname.split('/')[2] === 'introcoding') {
+    return introcoding;
+  } else if(window.location.pathname.split('/')[2] === 'deeplearning') {
+    return deeplearning;
+  } else if(window.location.pathname.split('/')[2] === 'reinforcementlearning') {
+    return reinfocementlearning;
   }
 }
 
