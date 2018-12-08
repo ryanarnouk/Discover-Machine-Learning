ca1fa648c3fcc906fc1cd2e6ee6123a65c673ebc
Ryan <azbo400@gmail.com>
2018-12-08 10:10:57 -0800

    Worked on more tests for the introcoding section

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -106,16 +106,19 @@ class Sidebar extends Component {
   }
 
   check = (event) => {
-    // currently the idea for running tests is to write code generators for all the blocks then check the outputs rather than using a ton of functions that get all the blocks in the workspace
-
-    // In the code generator we can run all the functions that are called and simply return all the values submitted. say there is a function return the values of it and then right here right tests to make sure that values are good taken from json for each challenge (assert code)
-
-
-    // run the code for the blocks here
     var code = this.props.blockly.JavaScript.workspaceToCode(this.props.blockly.getMainWorkspace());
-    code = code.split(','); //split if it has spaces(for array)
-    console.log(code);   
+    code = code.split(',') //split if it has spaces(for array)
+    // trim all the values to insure no spaces in each value
+    for (var i = 0; i < code.length; i++) {
+      code[i] = code[i].trim()
+    }
 
+    console.log(code);   
+    
+    this.setState({consoletext: code[0]});
+    if(code === "") {
+      this.setState({consoletext: "Nothing To Return"})
+    }
     // make tests work with multiple blockly components
     // there is a problem with this code that returns the test as true when it should be false
     var self = this;
@@ -127,6 +130,22 @@ class Sidebar extends Component {
         if(self.state.challengestate === true) {
           arrayofbool.push(false);
         }
+        arrayofbool.push(false);
+      }
+    }
+
+    function lengthfunc(a, b) {
+      if(a.length === 0) {
+        arrayofbool.push(false);
+      } else {
+        arrayofbool.push(true);
+      }
+    }
+
+    function doesnotequal(a, b) {
+      if(a !== b) {
+        arrayofbool.push(true);
+      } else {
         arrayofbool.push(false);
       }
     }
