bd844f0bde504ef357659f211cc78fd1e95e3e72
Ryan <azbo400@gmail.com>
2019-01-24 14:54:52 -0800

    Made it so opening a new window would not open a new tab

--- src/components/ChallengeList.js
+++ src/components/ChallengeList.js
@@ -12,7 +12,7 @@ import DeepLearning from '../seed/challenges/deeplearning/lesson.json';
 class SubList extends Component {
   render() {
     return (
-      <div style={{display: 'flex', alignItems: 'center', cursor: 'pointer', marginLeft: 50}} onClick={() => window.open(`/challenges/${this.props.section}/${this.props.data.number}`)}>
+      <div style={{display: 'flex', alignItems: 'center', cursor: 'pointer', marginLeft: 50}} onClick={() => window.location.href = `/challenges/${this.props.section}/${this.props.data.number}`}>
         {this.props.done ? (
           <CheckCircle/>
         ) : (
