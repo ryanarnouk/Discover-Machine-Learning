70f6442ca2a71697ba44ccc4308e0aba77b64851
Ryan <azbo400@gmail.com>
2019-03-03 14:48:38 -0800

    Added Global Game Context to React Context API

--- src/components/GameConnect/StartNewGame.js
+++ src/components/GameConnect/StartNewGame.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FontAwesome from 'react-fontawesome';
 import { withFirebase, FirebaseContext } from '../Firebase';
+import { GameContext }  from '../GameContext';
+import InGameLeaderboard from './InGameLeaderboard';
 
 class User extends Component {
   render() {
@@ -18,7 +20,8 @@ class StartNewGame extends Component {
     super(props);
 
     this.state = {
-      gamecode: ''
+      gamecode: '',
+      gamestart: false
     }
   }
 
@@ -27,44 +30,52 @@ class StartNewGame extends Component {
   }
 
   startgame = () => {
-    this.props.firebase.StartGame(this.state.gamecode, function () {
-      window.location.href = '/leaderboard';
+    this.props.firebase.StartGame(this.state.gamecode, () => {
+
     });
+    this.setState({gamestart: true})
   }
 
   render() { 
     return (  
-      <div style={{width: '100%', height: '100vh', backgroundColor: '#0C7CE8'}}>
-        <div style={{backgroundColor: '#0DC8FF', padding: 44}}>
-          <h1 style={{fontFamily: 'Rubik', color: 'white', margin: 0, textAlign: 'center', fontSize: '7em'}}>{this.state.gamecode}</h1>
-          <p style={{fontFamily: 'Rubik', color: 'white', textAlign: 'center', fontSize: '1.4em'}}>Type this game code in.</p>
-        </div>
-        <div style={{margin: 13, textAlign: 'center'}}>
-          <div style={{display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', gridGap: '1em'}}>
-            <User username="ryanarnouk" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-            <User username="azbo400" />
-          </div>
-          <button style={{margin: '0 auto', fontFamily: 'Rubik', border: 'none', padding: '16px 40px', background: 'rgb(255, 255, 255, 0.6)', cursor: 'pointer', borderRadius: '10px'}} onClick={this.startgame}>Start Game</button>
-          <p style={{color: 'white', fontFamily: 'Rubik', position: 'absolute', bottom: 0}}>31 students online</p>
-        </div>
-      </div>
+      <GameContext.Provider value={this.state}>
+        {this.state.gamestart ? 
+          <InGameLeaderboard />
+        :
+          <div style={{width: '100%', height: '100vh', backgroundColor: '#0C7CE8'}}>
+            <div style={{backgroundColor: '#0DC8FF', padding: 44}}>
+              <h1 style={{fontFamily: 'Rubik', color: 'white', margin: 0, textAlign: 'center', fontSize: '7em'}}><GameContext.Consumer>
+                {(context) => context.gamecode}
+              </GameContext.Consumer></h1>
+              <p style={{fontFamily: 'Rubik', color: 'white', textAlign: 'center', fontSize: '1.4em'}}>Type this game code in.</p>
+            </div>
+            <div style={{margin: 13, textAlign: 'center'}}>
+              <div style={{display: 'grid', gridTemplateColumns: 'repeat(6, 1fr)', gridGap: '1em'}}>
+                <User username="ryanarnouk" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+                <User username="azbo400" />
+              </div>
+              <button style={{margin: '0 auto', fontFamily: 'Rubik', border: 'none', padding: '16px 40px', background: 'rgb(255, 255, 255, 0.6)', cursor: 'pointer', borderRadius: '10px'}} onClick={this.startgame}>Start Game</button>
+              <p style={{color: 'white', fontFamily: 'Rubik', position: 'absolute', bottom: 0}}>31 students online</p>
+            </div>
+          </div> }
+      </GameContext.Provider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
@@ -6,14 +6,33 @@ import registerServiceWorker from './registerServiceWorker';
 import ReactGA from 'react-ga';
 
 import Firebase, { FirebaseContext } from './components/Firebase';
+import { GameContext }  from './components/GameContext';
 
 // Google Analytics
 ReactGA.initialize('UA-121847358-2');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
+class Providers extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gamecode: '123456'
+    }
+  }
+
+  render() {
+    return (
+      <GameContext.Provider value={this.state}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <App />
+        </FirebaseContext.Provider>
+      </GameContext.Provider>
+    );
+  }
+}
+
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <App />
-  </FirebaseContext.Provider>, 
+  <Providers />, 
   document.getElementById('root'));
 registerServiceWorker();
