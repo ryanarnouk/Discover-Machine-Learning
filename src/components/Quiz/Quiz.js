cbdb02982d5806224d9dea152525e3ac7f689e08
Ryan <azbo400@gmail.com>
2019-03-27 20:14:41 -0700

    Fix up quiz mode colours

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -18,14 +18,14 @@ class Quiz extends Component {
 
   render() {
     return (
-      <div style={{width: '100vw', height: '100vh', backgroundColor: '#BC00EB', fontFamily: 'Rubik'}}>
-        <div style={{padding: 10, textAlign: 'center', color: 'white', backgroundColor: '#CA46EB'}}>
+      <div style={{width: '100vw', height: '100vh', backgroundColor: '#8e24aa', fontFamily: 'Rubik'}}>
+        <div style={{padding: 10, textAlign: 'center', color: 'white', backgroundColor: '#ba68c8'}}>
           <strong><p style={{margin: 0}}>Quiz Mode</p></strong>
           <h1>{QuizJSON.questions[0].question}</h1>
         </div>
         <div style={{color: 'white', listStyle: 'none', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridGap: '3em', margin: 20}}>
           {QuizJSON.questions[0].options.map((a, i) => {
-            var colours = ["blue", "orange", "green", "red"];
+            var colours = ["#3d5afe", "#ff9100", "#00e676", "#f50057"];
             return (
               <Option color={colours[i]} text={a} key={i} />
             );
